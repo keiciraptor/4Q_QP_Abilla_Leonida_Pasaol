a9898482b4a8428a4f464d32bdd51e673cfa9d24
kei <143186747+keiciraptor@users.noreply.github.com>
2024-05-31 00:14:20 +0800

    updated

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
 function signup()
 {
-    var ufn = document.getElementById("fname").value;
+    var ufn = document.getElementById("fname").value; //takes value and stores it in the variable
     var umn = document.getElementById("mname").value;
     var uln = document.getElementById("lname").value;
     var umail = document.getElementById("email").value;
     var upass = document.getElementById("pword").value;
     var unum = document.getElementById("cnumber").value;
-    var usex = sex();
+    var usex = sexC();
     var usupp = document.getElementById("support").value;
     var ufoc = document.getElementById("foundoc").value;
 
-    if ((ufn == ""))||(umn == "")||(uln="")||(umail="")||(usupp=="")||(usex="")
+    if ((ufn == "")||(umn == "")||(uln="")||(umail="")||(usupp=="")||(usex="")||(upass =="")) //checks if there is any blank input
         {
             if (ufn == "")
                 {
-                    document.getElementById(".").innerHTML = "REQUIRED" ;
+                    document.getElementById("fnreq").innerHTML = "REQUIRED" ;
                 }
             if (umn == "")
                 {
-                    document.getElementById(".").innerHTML = "REQUIRED";
+                    document.getElementById("mnreq").innerHTML = "REQUIRED";
                 }
             if (uln == "")
                  {
-                    document.getElementById(".").innerHTML = "REQUIRED";
+                    document.getElementById("lnreq").innerHTML = "REQUIRED";
                  }        
             if (umail == "")
                 {
-                    document.getElementById(".").innerHTML = "REQUIRED";
+                    document.getElementById("mailreq").innerHTML = "REQUIRED";
                 }
             if(usupp == "")
                 {
-                    document.getElementById(".").innerHTML = "REQUIRED";
+                    document.getElementById("suppreq").innerHTML = "REQUIRED";
                 }
             if (usex == "")
                 {
-                    document.getElementById(".").innerHTML = "REQUIRED"; 
+                    document.getElementById("sexreq").innerHTML = "REQUIRED"; 
+                }
+            if (upass == "")
+                {
+                    document.getElementById("passreq").innerHTML = "REQUIRED";
                 }
         }
-        else
+        else //sets data for storing
         {
             localStorage.setItem("sfn", ufn);
             localStorage.setItem("smn", umn);
@@ -50,52 +54,62 @@ function signup()
         }
 }
 
-
-
 function display()
 {
-    var ufn = localStorage.getItem("sfn");
+    var ufn = localStorage.getItem("sfn"); //Takes the value and places in the mentioned variable
     var umn = localStorage.getItem("smn");
     var uln = localStorage.getItem("sln");
     var umail = localStorage.getItem("smail");
     var usex = localStorage.getItem("ssex");
     var usupp = localStorage.getItem("ssupp");
 
-    document.getElementById("displayfn").innerHTML += ufn;
-    document.getElementById("displaymn").innerHTML += umn;
-    document.getElementById("displayln").innerHTML += uln;
-    document.getElementById("displaymail").innerHTML += umail;
-    document.getElementById("displaysex").innerHTML += usex;
-    document.getElementById("displaysupp").innerHTML += usupp;
+    document.getElementById("displayfn").innerHTML += ufn;//Display First Name
+    document.getElementById("displaymn").innerHTML += umn; //Display Middle Name
+    document.getElementById("displayln").innerHTML += uln; //Display Last Name
+    document.getElementById("displaymail").innerHTML += umail; //Display Mail
+    document.getElementById("displaysex").innerHTML += usex; //Display Sex
+    document.getElementById("displaysupp").innerHTML += usupp; //Display Reason for supporting campaign
 
-    return false;
+    return false; //
 }
 
-function checkfn()
+function sexC() //Takes the choice of the user
 {
-    document.getElementById("").innerHTML="";
+    var sex="";
+    var sexC = document.getElementsByName("sex");
+    for (var ctr=0; ctr<sexC.length; ctr++){
+        if (sexC[ctr].checked){
+            sex += sexC[ctr].value;
+        }
+    }
+    return sex;
+}
+
+function checkfn()//Clears the required text once there is already input, same for the codes below.
+{
+    document.getElementById("fnreq").innerHTML="";
 }
 function checkmn()
 {
-    document.getElementById("").innerHTML="";
+    document.getElementById("mnreq").innerHTML="";
 }
 function checkln()
 {
-    document.getElementById("").innerHTML="";
+    document.getElementById("lnreq").innerHTML="";
 }
 function checkmail()
 {
-    document.getElementById("").innerHTML="";
+    document.getElementById("mailreq").innerHTML="";
 }
 function checksex()
 {
-    document.getElementById("").innerHTML="";
+    document.getElementById("sexreq").innerHTML="";
 }
 function checkpass()
 {
-    document.getElementById("").innerHTML="";
+    document.getElementById("passreq").innerHTML="";
 }
 function checksupp()
 {
-    document.getElementById("").innerHTML="";
+    document.getElementById("suppreq").innerHTML="";
 }
